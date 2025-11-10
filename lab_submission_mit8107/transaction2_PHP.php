76ea16ff483ccad0a81799dfb6bb28f64c17de29
Allan Omondi <aomondi@strathmore.edu>
2025-11-10 15:01:22 +0300

    Initial commit

---
+++ lab_submission_mit8107/transaction2_PHP.php
@@ -0,0 +1,9 @@
+<?php
+// Use the PHP web page as Transaction 2 (the one that reads the original data
+// and overwrites the changes made by Transaction 1).
+?>
+
+<!DOCTYPE html>
+<html>
+
+</html>
---
+++ lab_submission_mit8107/transaction2_python.py
@@ -0,0 +1,3 @@
+# Alternatively, if you prefer Python to PHP, you can use the Python script as
+# Transaction 2 (the one that reads the original data and overwrites the 
+# changes made by Transaction 1).
